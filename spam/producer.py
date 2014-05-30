239b224fac9483d40b59e519cafe4d0aabc4b86a
knd2 <gjz.knd2@gmail.com>
2014-05-30 10:20:47 +0800

    init spam

---
+++ spam/producer.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+LEVEL = (1, 2, 3, 4)
+METHODS = ("GET", "POST")
+CLINETS = (
+    "8.8.8.1",
+    "10.2.1.5",
+    "8.1.1.4",
+    "8.1.1.8",
+    "80.1.1.8",
+    "78.3.1.4",
+    "68.1.30.4",
+    "8.3.1.4",
+    "8.1.30.4",
+    "8.14.1.4"
+)
+
+def producer():
+    pass
+
+def ramdom_time():
+    pass
+
+def random_url():
+    pass
+
+def response_time():
+    pass
