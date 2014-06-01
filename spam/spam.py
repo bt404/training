c01491702e3bf1c83ad190bd417c0b603c09fcae
knd2 <gjz.knd2@gmail.com>
2014-06-01 11:32:41 +0800

    init model&spam

--- spam/spam.py
+++ spam/spam.py
@@ -1,5 +1,60 @@
 # -*- coding: utf-8 -*-
 # 实现一个防垃圾信息的简易功能
 # 1. 生成格式化的日志文件（已经有producer.py实现，日志文件在data/access.log）
-# 2. 找到半小时内访问topic超过两次的用户列表
-# 3. 找到一个路径列表，其中每个路径至少被两个用户访问
+# 2. 找到半小时内访问topic超过两次且至少有两个不同page的用户列表
+# 3. 找到一个路径列表，其中每个路径每天至少被两个用户访问
+
+
+from model import Record
+from datetime import datetime, timedelta
+import time
+import os
+import re
+
+PATTERN = r'(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})]\s([A-Za-z]+)\s.+(/[a-z]+/\d{3})\s(\d+\.\d+\.\d+\.\d+)'
+
+def line_parser(line):
+    pattern = re.compile(PATTERN)
+    #time, name, view, ip = pattern.findall(line)[0]
+    return pattern.findall(line)[0]
+
+
+def save_info(file_name):
+    if not os.path.exists(file_name):
+        return 'file not exists'
+    Record.dropCollection()
+    with open(file_name) as file:
+        for line in file.readlines():
+            result = Record.create()
+            time, name, view, ip = line_parser(line)
+            result["ip"] = ip
+            result["name"] = name
+            result["view"] = view
+            resp_time = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S")
+            result["time"] = resp_time
+            result.save()
+
+
+def get_users():
+    now = datetime.now()
+    cond_time = now-timedelta(15)
+    cond = {}
+    cond["$and"] = [{"time": {"$gt": cond_time}}, {"time": {"$lt": now}}]
+    result = Record.findDocs(cond)
+    #print result.count()
+    result = Record.aggregate([
+        {"$match": cond},
+        {"$group": {"_id": {"ip": "$ip", "view": "$view"}, "num": {"$sum": 1}}},
+        {"$sort": {"num": -1}}
+    ])
+    for user, num in result:
+        pass
+    print result
+
+
+def get_views():
+    pass
+
+
+save_info('data/access.log')
+get_users()
